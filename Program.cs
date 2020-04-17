32310bcd471047d45ce9f549255069bf1161bedb
Austin Parvin <austin.parvin@gmail.com>
2020-04-17 17:02:31 -0400

    updated view ordered by heaviest

--- Program.cs
+++ Program.cs
@@ -1,70 +1,72 @@
 ﻿using System;
+using System.Linq;
 
 namespace JurassicPark
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      var d = new DinosaurManager();
-      var userInput = "";
-      Console.WriteLine("                                                                                                                                            ");
-      Console.WriteLine(@"         ,---._                                                                                     ,-.----.                                 ");
-      Console.WriteLine(@"       .-- -.' \                                                                                    \    /  \                           ,-.  ");
-      Console.WriteLine(@"       |    |   :                                                          ,--,                     |   :    \                      ,--/ /|  ");
-      Console.WriteLine(@"       :    ;   |         ,--,  __  ,-.                                  ,--.'|                     |   |  .\ :            __  ,-.,--. :/ |  ");
-      Console.WriteLine(@"       :        |       ,'_ /|,' ,'/ /|             .--.--.    .--.--.   |  |,                      .   :  |: |          ,' ,'/ /|:  : ' /   ");
-      Console.WriteLine(@"       |    :   :  .--. |  | :'  | |' | ,--.--.    /  /    '  /  /    '  `--'_       ,---.          |   |   \ : ,--.--.  '  | |' ||  '  /    ");
-      Console.WriteLine(@"       :         ,'_ /| :  . ||  |   ,'/       \  |  :  /`./ |  :  /`./  ,' ,'|     /     \         |   : .   //       \ |  |   ,''  |  :    ");
-      Console.WriteLine(@"       |    ;   ||  ' | |  . .'  :  / .--.  .-. | |  :  ;_   |  :  ;_    '  | |    /    / '         ;   | |`-'.--.  .-. |'  :  /  |  |   \   ");
-      Console.WriteLine(@"   ___ l         |  | ' |  | ||  | '   \__\/: . .  \  \    `. \  \    `. |  | :   .    ' /          |   | ;    \__\/: . .|  | '   '  : |. \  ");
-      Console.WriteLine(@" /    /\    J   ::  | : ;  ; |;  : |   ,' .--.; |   `----.   \ `----.   \'  : |__ '   ; :__         :   ' |    ,' .--.; |;  : |   |  | ' \ \ ");
-      Console.WriteLine(@"/  ../  `..-    ,'  :  `--'   \  , ;  /  /  ,.  |  /  /`--'  //  /`--'  /|  | '.'|'   | '.'|        :   : :   /  /  ,.  ||  , ;   '  : |--'  ");
-      Console.WriteLine(@"\    \         ; :  ,      .-./---'  ;  :   .'   \'--'.     /'--'.     / ;  :    ;|   :    :        |   | :  ;  :   .'   \---'    ;  |,'     ");
-      Console.WriteLine(@" \    \      ,'   `--`----'          |  ,     .-./  `--'---'   `--'---'  |  ,   /  \   \  /         `---'.|  |  ,     .-./        '--'       ");
-      Console.WriteLine(@"  '---....--'                         `--`---'                            ---`-'    `----'            `---`   `--`---'                       ");
-      Console.WriteLine("                                                                                                                                              ");
-
-      while (userInput != "q")
-      {
-        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        Console.WriteLine("What would you like to do:");
-        Console.WriteLine("(VIEW 'v'), (ADD 'a'), (REMOVE 'r'), (TRANSFER 't'), (VIEW HEAVIEST 'h'), (DIET COUNT 'd'), (QUIT 'q')");
-        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        Console.WriteLine("");
-        Console.WriteLine("What would you like to do:");
-        userInput = Console.ReadLine();
-        Console.WriteLine("");
-        switch (userInput)
+        static void Main(string[] args)
         {
-          case "v":
-            d.View(d.Dinosaurs);
-            break;
+            var d = new DinosaurManager();
+            var userInput = "";
+            Console.WriteLine("                                                                                                                                            ");
+            Console.WriteLine(@"         ,---._                                                                                     ,-.----.                                 ");
+            Console.WriteLine(@"       .-- -.' \                                                                                    \    /  \                           ,-.  ");
+            Console.WriteLine(@"       |    |   :                                                          ,--,                     |   :    \                      ,--/ /|  ");
+            Console.WriteLine(@"       :    ;   |         ,--,  __  ,-.                                  ,--.'|                     |   |  .\ :            __  ,-.,--. :/ |  ");
+            Console.WriteLine(@"       :        |       ,'_ /|,' ,'/ /|             .--.--.    .--.--.   |  |,                      .   :  |: |          ,' ,'/ /|:  : ' /   ");
+            Console.WriteLine(@"       |    :   :  .--. |  | :'  | |' | ,--.--.    /  /    '  /  /    '  `--'_       ,---.          |   |   \ : ,--.--.  '  | |' ||  '  /    ");
+            Console.WriteLine(@"       :         ,'_ /| :  . ||  |   ,'/       \  |  :  /`./ |  :  /`./  ,' ,'|     /     \         |   : .   //       \ |  |   ,''  |  :    ");
+            Console.WriteLine(@"       |    ;   ||  ' | |  . .'  :  / .--.  .-. | |  :  ;_   |  :  ;_    '  | |    /    / '         ;   | |`-'.--.  .-. |'  :  /  |  |   \   ");
+            Console.WriteLine(@"   ___ l         |  | ' |  | ||  | '   \__\/: . .  \  \    `. \  \    `. |  | :   .    ' /          |   | ;    \__\/: . .|  | '   '  : |. \  ");
+            Console.WriteLine(@" /    /\    J   ::  | : ;  ; |;  : |   ,' .--.; |   `----.   \ `----.   \'  : |__ '   ; :__         :   ' |    ,' .--.; |;  : |   |  | ' \ \ ");
+            Console.WriteLine(@"/  ../  `..-    ,'  :  `--'   \  , ;  /  /  ,.  |  /  /`--'  //  /`--'  /|  | '.'|'   | '.'|        :   : :   /  /  ,.  ||  , ;   '  : |--'  ");
+            Console.WriteLine(@"\    \         ; :  ,      .-./---'  ;  :   .'   \'--'.     /'--'.     / ;  :    ;|   :    :        |   | :  ;  :   .'   \---'    ;  |,'     ");
+            Console.WriteLine(@" \    \      ,'   `--`----'          |  ,     .-./  `--'---'   `--'---'  |  ,   /  \   \  /         `---'.|  |  ,     .-./        '--'       ");
+            Console.WriteLine(@"  '---....--'                         `--`---'                            ---`-'    `----'            `---`   `--`---'                       ");
+            Console.WriteLine("                                                                                                                                              ");
 
-          case "a":
-            d.Add();
-            break;
+            while (userInput != "q")
+            {
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("What would you like to do:");
+                Console.WriteLine("(VIEW 'v'), (ADD 'a'), (REMOVE 'r'), (TRANSFER 't'), (VIEW ORDERED BY HEAVIEST 'h'), (DIET COUNT 'd'), (QUIT 'q')");
+                Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                Console.WriteLine("");
+                Console.WriteLine("What would you like to do:");
+                userInput = Console.ReadLine();
+                Console.WriteLine("");
+                switch (userInput)
+                {
+                    case "v":
+                        d.View(d.Dinosaurs);
+                        break;
 
-          case "r":
-            d.Remove();
-            break;
+                    case "a":
+                        d.Add();
+                        break;
 
-          case "t":
-            d.Transfer();
-            break;
+                    case "r":
+                        d.Remove();
+                        break;
 
-          case "h":
-            d.Heaviest();
-            break;
+                    case "t":
+                        d.Transfer();
+                        break;
 
-          case "d":
-            d.DietCount();
-            break;
+                    case "h":
+                        d.Heaviest();
+                        break;
 
-          case "q":
-            break;
+                    case "d":
+                        d.DietCount();
+                        break;
+
+                    case "q":
+                        break;
+                }
+            }
         }
-      }
     }
-  }
 }
+
